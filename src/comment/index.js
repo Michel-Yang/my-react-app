5bbc07764a995cdee117c76120f90bc0e5cb6e78
Michel-Yang <18826220374@163.com>
2020-02-05 19:22:02 +0800

    表单提交

--- src/comment/index.js
+++ src/comment/index.js
@@ -13,14 +13,46 @@ export default class CmtList extends React.Component{
                 {id:3,user:"赵六",content:"哈哈！123"},
                 {id:4,user:"时代",content:"哈哈！123"},
                 {id:5,user:"中国",content:"哈哈！123"}
-            ]
+            ],
+
+            text:""
+            
+
         }
+    }
+    txtChange=(e)=>{
+        let value=e.target.value;
+        this.setState({
+            text:value
+        });
+
+    }
+
+    submit=()=>{
+
+        var obj={
+            id:6,
+            user:"中国",
+            content:""
+        }
+
+        obj.content=this.state.text;
+
+        this.state.CommentList.push(obj)
+
+        console.log(this.state.CommentList)
+
     }
     render(){
 
         return <div>
            <h1 className='title'>这是评论列表组件</h1>
            { this.state.CommentList.map(item=><CmtItem {...item} key={item.id}></CmtItem>)}
+
+           <div className='comment'>
+               <input type="text" value={this.state.text} onChange={this.txtChange} ></input>
+               <button onClick={this.submit}> 提交</button>
+           </div>
         </div>
     }
 
